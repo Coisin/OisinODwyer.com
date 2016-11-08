d3f80f1957bcc26ccb2c06b520ba8e0f37cca2b5
coisin <odwyeroisin@gmail.com>
2016-11-08 16:56:57 +0000

    Added Tables project, and improved JS for overview

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ totalFadeHeight = 300; //px
 initialOpacity = ratio = .97;
 
 $(function() {
-
   setOverlayOpacity();
 
   $(window).scroll(function() {
@@ -12,6 +11,12 @@ $(function() {
 
 function setOverlayOpacity() {
   var distanceToTop = $(".overlay").offset().top;
-  ratio =  initialOpacity - (distanceToTop / totalFadeHeight)
+  ratio =  initialOpacity - (distanceToTop / totalFadeHeight);
+
+  if(ratio <= 0) {
+    $(".overlay").css("pointer-events", "none");
+  } else {
+    $(".overlay").css("pointer-events", "");
+  }
   $(".overlay").css("opacity", ratio);
 }
