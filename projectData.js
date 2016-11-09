6288c091fd3f855b46acbc663a3391f5654af459
coisin <odwyeroisin@gmail.com>
2016-11-09 01:18:17 +0000

    Refactored everything and created Project.html template

---
+++ projectData.js
@@ -0,0 +1,105 @@
+projectData = {
+    tables: {
+    name: "Tables",
+    description: `Tables is an implementation of the Java JTable class, which allows you to easily bind data structures
+    to tables in your Java Swing applications.`,
+    links: ["https://www.github.com/coisin/javatables", "https://github.com/Coisin/JavaTables/releases/latest"],
+    linkDisplays: ["Github.com/Coisin/JavaTables", "Download"],
+    stylesheet: "stylesheets/projects/tables.css",
+    favicon: "",
+    numRows: 3,
+    rows: [
+      {},
+      {},
+      {
+        content: `<div class="code-info">
+        <h4>The above Table was made using Java Tables, with the following, simple line of code</h4>
+      </div>
+      <pre class="code-block">
+        <code class="language-java">
+          Table&ltSeries&gt seriesTable = new Table(Series.class);</code>
+      </pre>
+      <div class="code-info">
+        <h4>Here, <code>Series</code> is a simple class:</h4>
+      </div>
+      <pre class="code-block"><code class="language-java">
+          public class Series {
+            public String name, link, service;
+            public int progress = 0;
+
+            public Series(String name, String link, String service) {
+              this.name = name;
+              this.link = link;
+              this.service = service;
+            }
+          }
+        </code></pre>
+        <div class="code-info">
+          <h4>In order to ignore the <code>progress</code> field, we can use the following line of code: </h4>
+        </div>
+        <pre class="code-block"><code class="language-java">
+          seriesTable.avoidColumn("progress");
+        </code></pre>
+        <div class="code-info">
+          <h4>Last of all, in order to add rows to the table, all we must do is create a <code>Collection&ltSeries&gt</code>, and call
+            the <code>addManyRows()</code> method:</h4>
+        </div>
+        <pre class="code-block">
+          <code class="language-java">
+            ArrayList&ltSeries&gt series = new ArrayList(); /* ArrayList implements Collection&ltE&gt */
+            series.add(new Series("Death Note", "http://www.mangareader.net/death-note", "MangaReader"));
+
+            seriesTable.addManyRows(series);
+          </code>
+        </pre>`
+      }
+    ]
+  },
+  raptor: {
+  name: "MangaRaptor",
+  description: `MangaRaptor is an application built in Java, which allows you to download unlimited amounts of Manga,
+onto your computer, in the form of picture files, all for free! The images are scraped from a number of websites, such as MangaReader.net
+and Manga3.net.`,
+  links: ["https://www.github.com/Coisin/MangaRaptor", "https://github.com/Coisin/MangaRaptor/releases/latest"],
+  linkDisplays: ["Github.com/Coisin/MangaRaptor", "Download"],
+  stylesheet: "stylesheets/projects/mangaraptor.css",
+  favicon: "resources/mangaraptor-2.png",
+  numRows: 4,
+  rows: [
+    {},
+    {},
+    {},
+    {
+      content: `    <h1>How it works?</h1>
+          <h4>If you want to download individual chapters, or all chapters of a manga series, MangaRaptor enables you to do so.</h4>
+          <ul class="instruction-list">
+            <li>Upon opening the application, type the name of the Manga Series into the Search Text Field, and click search. This
+                will open a tabbed layout of the various Manga Downloading Services.
+            </li>
+            <li>Navigate through the tabs, to find the series which you are looking for. Click it, and click the "Add" button.</li>
+            <li>All the chapters for the selected Manga Series will open in the Chapters Table. You can then select all the individual chapters
+                you wish to download, or click the "Select All" button beneath the table. Selected multiple items can be done by
+                holding Control, and clicking on each chapter. Once finished, click the "Add To Downloads" button.</li>
+            <li>Between the two tables, there is a button for selecting a destination directory. Click this and selectory a directory,
+                in which to download your Manga.</li>
+            <li>Click the "Download" button beneath the Downloads Table.</li>
+          </ul>`
+    }
+  ]
+  },
+  examli: {
+  name: "Examli",
+  description: `Examli is an online application, written in Ruby On Rails, which is to help students study, in a quiz like
+  manor. I was one of two people who contributed to this project (The other being <a href="http://www.gytdau.com/">Gytis Daujotas</a>).`,
+  links: ["http://www.exam.li/"],
+  linkDisplays: ["Exam.li"],
+  stylesheet: "stylesheets/projects/examli.css",
+  favicon: "",
+  numRows: 3,
+  rows: [
+    {},
+    {},
+    {}
+  ]
+  },
+}
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,43 @@
 $(function() {
-  initialOpacity = .9;
+  preProject();
+  initializeLayout();
+  postInitialization();
+});
+
+function preProject() {
+  projectType = getQueryVariable("p");
+  project = projectData[projectType];
+}
+
+function initializeLayout() {
+  $(".project-title").text(project.name);
+  $(".project-description").html(project.description);
+  $(".project-favicon").attr("href", project.favicon);
+  for(var i = 0;i < project.links.length;i++) {
+    $(".availability-table").append("<tr><td><a href='"+project.links[i]+"' class='project-link'>"+project.linkDisplays[i]+"</a></td></tr>");
+  }
+  $(".project-stylesheet").attr("href", project.stylesheet);
+  for(var i = 1;i <= project.numRows;i++) {
+    $(".container-fluid").append("<div class='row hero"+(i % 2 == 0?" scroll-fixed":"")+"' id='"+projectType+"-"+i+"'></div>");
+    $("#"+projectType+"-"+i).html(project.rows[i - 1].content);
+  }
+}
+
+function postInitialization() {
+  Prism.highlightAll();
+
   totalFadeHeight = 400;
   setOverlayOpacity();
-});
+
+}
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
