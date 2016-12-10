a0623905bd6704e0fe3f7e205d675283238cd43a
coisin <odwyeroisin@gmail.com>
2016-12-10 22:48:45 +0000

    Added some more typing animations

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
 
+var projectTypes = ["Unique", "Creative", "Small", "Large"];
+var projectTypeIndex = 0;
+
+var secondNames = ["O'Duibhir", "O'Dwyer"];
+var secondNameIndex = 0;
+
 $(function() {
   initializeLayout();
 });
@@ -11,4 +17,43 @@ function initializeLayout() {
     numberOfProjects++;
     $(".project-list").append("<tr><td><a href='project.html?p="+i+"' class='project-listing grey-trans'>" + projectData[i].name + "</a></td></tr>");
   }
+  shuffle(projectTypes);
+  shuffle(secondNames);
+  setInterval(updateTransitions, 5000);
+}
+
+function updateTransitions() {
+  projectTypeIndex++;
+  if(projectTypeIndex >= projectTypes.length) {
+    projectTypeIndex = 0;
+  }
+  var projectTypeName = $("#project-type-name");
+  projectTypeName.fadeOut(function() {
+    projectTypeName.remove()
+    .appendTo($(".project-type-container"))
+    .text(projectTypes[projectTypeIndex])
+    .fadeIn();
+  });
+  secondNameIndex++;
+
+  if(secondNameIndex >= secondNames.length) {
+    secondNameIndex = 0;
+  }
+  var secondName = $("#second-name");
+  secondName.fadeOut(function() {
+    secondName.remove()
+    .appendTo($(".second-name-container"))
+    .text(secondNames[secondNameIndex])
+    .fadeIn();
+  });
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
 }
